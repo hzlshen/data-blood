bdc4a0b655cb07b5ab07bf1310665ca640da0a02
hzlshen <m15277968105@163.com>
2017-12-09 14:30:02 +0800

    what?

--- vue-hello/src/App.vue
+++ vue-hello/src/App.vue
@@ -1,23 +1,43 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <Row>
+    <Col span="18" offset="3">
+    <Menu mode="horizontal"  active-name="1">
+      <Menu-item name="1">
+        <Icon type="ios-paper"></Icon>
+        <router-link to="/home">首页</router-link>
+      </Menu-item>
+      <Menu-item name="2">
+        <Icon type="ios-people"></Icon>
+        <router-link to="/list">书单</router-link>
+      </Menu-item>
+      <Menu-item name="3">
+        <Icon type="ios-people"></Icon>
+        <router-link to="/manger/add">管理图书</router-link>
+      </Menu-item>
+      <Menu-item name="4">
+        <Icon type="ios-cart-outline"></Icon>
+        <router-link to="/cart">购物车</router-link>
+        <Badge :count="num" class-name="demo-badge-alone" ></Badge>
+      </Menu-item>
+    </Menu>
+    <div>
+    </div>
+    <div><router-view></router-view></div>
+    </Col>
+  </Row>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import {mapGetters} from 'vuex'
+  export default {
+    name: 'app',
+    computed: {
+      ...mapGetters({
+        buyBooks: 'buyBooks'
+      }),
+      num(){
+        return this.buyBooks.length
+      }
+    }
+  }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- vue-hello/src/router/index.js
+++ vue-hello/src/router/index.js
@@ -1,60 +1,54 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import Home from '../components/Hone.vue'
+import Home from '../components/Home.vue'
 import List from '../components/List.vue'
-import Manger from '../components/Manger.vue';
-import Update from '../components/Update'
 import Add from '../components/Add.vue'
-import Delete from '../components/Delete.vue'
+import Manger from '../components/Manger.vue'
 import Detail from '../components/Detail.vue'
+import Update from '../components/Update.vue'
+import Delete from '../components/Delete.vue'
+import Cart from '../components/Cart.vue'
 
-Vue.use(Router)
+Vue.use(Router);
+
+const routes = [
+  {path:'/home',
+    component:Home
+  },
+  {
+    path:'/list',
+    component:List
+  },
+  {
+    path:'/manger',
+    component:Manger,
+    children:[
+      {
+        path:'add',
+        component:Add
+      },
+      {
+        path:'update',
+        component:Update
+      },
+      {
+        path:'delete',
+        component:Delete
+      }
+
+    ]
+  },
+  {
+    path:'/detail/:id',
+    name:'detail',
+    component:Detail
+  },
+  {
+    path:'/cart',
+    component:Cart
+  }
+]
 
 export default new Router({
-  routes: [
-    {
-      path: '/',  //主路由
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/Home',  //主路由
-      name: 'Home',
-      component: Home
-    },
-    {
-      path: '/List',
-      name: 'List',
-      component: List
-    },
-    {
-      path: '/Manger',
-      name: 'Manger',
-      component: Manger,
-      children:[
-        {
-          path:'/Add',
-          name:'Add',
-          component:'Add'
-        },
-        {
-          path: '/Update',
-          name: 'Update',
-          component: Update
-        },
-        {
-          path:'/Delete',
-          name:'Delete',
-          component:'Delete'
-        }
-      ]
-    },
-    {
-      path:'/Detail',
-      name:'Detail',
-      component:'Detail'
-    }
-
-  ]
+  routes
 })
