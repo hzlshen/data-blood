5f7f9989d202aac4bcff791bd784a8fdf93e2ca9
hzlshen <m15277968105@163.com>
2018-10-26 22:23:33 +0800

    feat(*):优化

--- src/axios/api.js
+++ src/axios/api.js
@@ -16,12 +16,3 @@ export const getSelect = () => fetch({//lookOption是你要调用接口的名字
   params:{
   }
 });
-
-// export const lookOption = (username,password,captcha) => fetch({//lookOption是你要调用接口的名字，issuer,userId是传进来的参数
-//   //api.getData 引用url.js里面的数据
-//   url: api.getData,
-//   method: 'get',//请求方法
-//   params:{
-//      username,password,captcha//参数
-//   }
-// });
