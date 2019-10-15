75ab276dd3765fdf696ee2a0804425f97def91f9
hzlshen <m15277968105@163.com>
2019-10-15 14:45:11 +0800

    update index import

--- src/components/index.vue
+++ src/components/index.vue
@@ -8,11 +8,17 @@
  <div class="index-home">
    <el-tabs v-model="activeName" @tab-click="handleClick">
       <el-tab-pane label="echart" name="echart">
-        
+        <echart-type></echart-type>
       </el-tab-pane>
-      <el-tab-pane label="jsPlumb" name="jsPlumb">配置管理</el-tab-pane>
-      <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
-      <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
+      <el-tab-pane label="jsPlumb" name="jsPlumb">
+        <jsplumb-type></jsplumb-type>
+      </el-tab-pane>
+      <el-tab-pane label="d3" name="d3">
+        <d3-type></d3-type>
+      </el-tab-pane>
+      <el-tab-pane label="cyroscape" name="cyroscape">
+        <cytoscape-type></cytoscape-type>
+      </el-tab-pane>
     </el-tabs>
  </div>
 
@@ -20,8 +26,17 @@
 </template>
 
 <script>
-
+import echartType from './type/echart'
+import d3Type from './type/d3'
+import jsplumbType from './type/jsplumb'
+import cytoscapeType from './type/cytoscape'
 export default {
+  components: {
+    echartType,
+    d3Type,
+    jsplumbType,
+    cytoscapeType
+  },
   data () {
     return {
       activeName: 'echart'
