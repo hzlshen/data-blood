a2ec7019a66e8a4761db4568a3a2945e7a2eaefe
hzlshen <m15277968105@163.com>
2019-10-15 15:37:52 +0800

    npm install echart

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router/index'
 import Elementui from 'element-ui'
 import axios from 'axios'
+
+import Echarts from "echarts";
+ 
+Vue.prototype.$echarts = Echarts;
+ 
+Vue.use(Echarts)
 import 'element-ui/lib/theme-chalk/index.css'//引入css
 Vue.use(Elementui)
 Vue.prototype.$http = axios;
